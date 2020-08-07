df550787fd6b82af3561bc868dc30bd6dbfb89e5
Song Yang <song.yang@signify.com>
2020-08-07 10:53:14 +0800

    这是branch-1的第二次修改

--- src/main/java/com/yangsong/jenkins_test/server/UserService.java
+++ src/main/java/com/yangsong/jenkins_test/server/UserService.java
@@ -26,6 +26,7 @@ public class UserService {
         System.out.println("这是2个小测试........");
         System.out.println("这是3个小测试........");
         System.out.println("这是branch-1的第一次修改........");
+        System.out.println("这是branch-1的第二次修改........");
 
 
         return "hello world!!!!";
